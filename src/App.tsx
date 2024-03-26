4f300e8c6fe9a481ee08fdd7d1152fdcd2945a71
acz19 <acz1992@hotmail.co.uk>
2024-03-26 21:28:15 +0100

    Set up to where I was before

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
-import "./index.css";
-
+import Header from "./components/Header";
+import AnimRoutes from "./components/AnimRoutes";
+import { BrowserRouter as Router } from "react-router-dom";
+/* import { motion } from "framer-motion";
+ */
 function App() {
-	return <div className="text-xl">Hello World</div>;
+	return (
+		<>
+			<Router>
+				<Header />
+				<AnimRoutes />
+			</Router>
+		</>
+	);
 }
 
 export default App;
