8cc55fa2704de41e48e6c6240c1f716fd8449467
acz19 <acz1992@hotmail.co.uk>
2024-03-27 22:55:19 +0100

    Add Navigation overlay

--- src/components/MobileNav.tsx
+++ src/components/MobileNav.tsx
@@ -1,8 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
+import { IoMdClose } from "react-icons/io";
+import { CgMenuRight } from "react-icons/cg";
+import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
+import { Variants } from "../utils/Props";
+
+// Menu Variants
+
+const menuVariants: Variants = {
+	hidden: {
+		x: "100%",
+	},
+	show: {
+		x: 0,
+		transition: {
+			ease: [0.6, 0.01, -0.05, 0.9],
+		},
+	},
+};
 
 const MobileNav = () => {
-	return <div>MobileNav</div>;
+	const [openMenu, setOpenMenu] = useState(false);
+	return (
+		<nav className="text-primary xl:hidden">
+			{/* Nav open Button */}
+			<div
+				onClick={() => setOpenMenu(true)}
+				className="text-3xl cursor-pointer"
+			>
+				<CgMenuRight />
+			</div>
+			{/* Menu */}
+			<motion.div
+				variants={menuVariants}
+				initial="hidden"
+				animate={openMenu ? "show" : ""}
+				className="bg-white shadow-2xl w-full absolute top-0 right-0 mx-w-xs h-screen z-20"
+			>
+				{/* Icon */}
+				<div
+					onClick={() => setOpenMenu(false)}
+					className="text-4xl absolute z-30 left-4 top-14 text-primary cursor-pointer"
+				>
+					<IoMdClose />
+				</div>
+				{/* Menu List */}
+				<ul className="h-full flex flex-col justify-center items-center gap-y-8  text-primary font-primary font-bold text-3xl ">
+					<li>
+						<Link to="/">Home</Link>
+					</li>
+					<li>
+						<Link to="/about">About</Link>
+					</li>
+					<li>
+						<Link to="/portfolio">Portfolio</Link>
+					</li>
+					<li>
+						<Link to="/contact">Contact</Link>
+					</li>
+				</ul>
+			</motion.div>
+		</nav>
+	);
 };
 
 export default MobileNav;
-MobileNav;
--- src/components/Socials.tsx
+++ src/components/Socials.tsx
@@ -1,8 +1,38 @@
 import React from "react";
+import {
+	ImFacebook,
+	ImTwitter,
+	ImPinterest,
+	ImInstagram,
+} from "react-icons/im";
 
 const Socials = () => {
-	return <div>Socials</div>;
+	return (
+		<div className="hidden xl:flex ml-24">
+			<ul>
+				<li>
+					<a href="http://www.facebook.com" target="_blank">
+						<ImFacebook />
+					</a>
+				</li>
+				<li>
+					<a href="http://www.twitter.com" target="_blank">
+						<ImTwitter />
+					</a>
+				</li>
+				<li>
+					<a href="http://www.pinterest.com" target="_blank">
+						<ImPinterest />
+					</a>
+				</li>
+				<li>
+					<a href="http://www.instagram.com" target="_blank">
+						<ImInstagram />
+					</a>
+				</li>
+			</ul>
+		</div>
+	);
 };
 
 export default Socials;
-Socials;
