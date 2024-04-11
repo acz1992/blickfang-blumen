cf96f435e0eeb7a1e848a8ca5b340d663383a57b
acz19 <acz1992@hotmail.co.uk>
2024-04-11 17:48:21 +0100

    Add Email.js

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -1,27 +1,100 @@
+import { useEffect, useRef, useState } from "react";
+import emailjs from "emailjs-com";
+
 const ContactForm = () => {
+	const form = useRef<HTMLFormElement>(null);
+	const [emailSubmitted, setEmailSubmitted] = useState(false);
+
+	const sendEmail = (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+
+		emailjs
+			.sendForm(
+				import.meta.env.REACT_APP_SERVICE_ID,
+				import.meta.env.REACT_APP_TEMPLATE_ID,
+				form.current!,
+				import.meta.env.REACT_APP_PUBLIC_KEY
+			)
+			.then(
+				() => {
+					console.log("SUCCESS!");
+					setEmailSubmitted(true);
+				},
+				(error) => {
+					console.log("FAILED...", error.text);
+				}
+			);
+		if (form.current) form.current.reset(); // Checking if form.current is not null before calling reset
+	};
+
+	// Overlay confirming Form Submission
+	useEffect(() => {
+		if (emailSubmitted) {
+			const handleOutsideClick = (e: MouseEvent) => {
+				if (form.current && !form.current.contains(e.target as Node)) {
+					setEmailSubmitted(false);
+				}
+			};
+
+			document.addEventListener("mousedown", handleOutsideClick);
+
+			return () => {
+				document.removeEventListener("mousedown", handleOutsideClick);
+			};
+		}
+	}, [emailSubmitted]);
+
 	return (
-		<form className="flex flex-col gap-y-4">
-			<div className="flex gap-x-10">
+		<>
+			<form
+				className="flex flex-col gap-y-4"
+				ref={form}
+				onSubmit={sendEmail}
+			>
+				<div className="flex gap-x-10">
+					<input
+						className="outline-none border-b border-b-primary h-[60px] bg-transparent font-secondary w-full pl-3 placeholder:text-primary"
+						type="text"
+						placeholder="Your name"
+						name="name"
+						id="name"
+						required
+					/>
+					<input
+						className="outline-none border-b border-b-primary h-[60px] bg-transparent font-secondary w-full pl-3 placeholder:text-primary"
+						type="email"
+						placeholder="Your email address"
+						name="email"
+						id="email"
+						required
+					/>
+				</div>
 				<input
-					className="outline-none border-b border-b-primary h-[60px] bg-transparent font-secondary w-full pl-3 placeholder:text-primary"
+					className="outline-none border-b border-b-primary h-[60px] bg-transparent font-secondary w-full pl-3"
 					type="text"
-					placeholder="Your name"
+					placeholder="Your message"
+					name="message"
+					id="message"
+					required
 				/>
-				<input
-					className="outline-none border-b border-b-primary h-[60px] bg-transparent font-secondary w-full pl-3 placeholder:text-primary"
-					type="text"
-					placeholder="Your email address"
-				/>
-			</div>
-			<input
-				className="outline-none border-b border-b-primary h-[60px] bg-transparent font-secondary w-full pl-3"
-				type="text"
-				placeholder="Your message"
-			/>
-			<button className="btn mb-[30px] mt-5 mx-auto lg:mx-0 self-start">
-				Send it
-			</button>
-		</form>
+				<button
+					type="submit"
+					className="btn mb-[30px] mt-5 mx-auto lg:mx-0 self-start"
+				>
+					Send it
+				</button>
+			</form>
+
+			{emailSubmitted && (
+				<div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 flex justify-center items-center">
+					<div className="bg-white p-5 rounded-lg">
+						<p className="text-green-500  text-base font-bold mt-2">
+							I&apos;ll be in touch soon
+						</p>
+					</div>
+				</div>
+			)}
+		</>
 	);
 };
 
