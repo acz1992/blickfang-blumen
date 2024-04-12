bb9d0031c7b0287068e5b25a94bbce3cd0303b5b
acz19 <acz1992@hotmail.co.uk>
2024-04-12 10:18:40 +0100

    Animation added to header on hover

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,9 +4,22 @@ import Logo from "../assets/img/header/blicklogo.png";
 import MobileNav from "./MobileNav";
 import { Link } from "react-router-dom";
 
-const Header = () => {
+interface NavLink {
+	to: string;
+	text: string;
+}
+
+const Header: React.FC = () => {
+	// Define an array of objects representing navigation links
+	const navLinks: NavLink[] = [
+		{ to: "/", text: "Home" },
+		{ to: "/about", text: "About" },
+		{ to: "/portfolio", text: "Portfolio" },
+		{ to: "/contact", text: "Contact" },
+	];
+
 	return (
-		<header className=" fixed w-full px-[30px] lg:px-[100px] z-30 h-[100px] lg:h-[140px] flex items-center">
+		<header className="fixed w-full px-[30px] lg:px-[100px] z-30 h-[100px] lg:h-[140px] flex items-center">
 			<div className="flex flex-col lg:flex-row lg:items-center w-full justify-between">
 				{/* Logo */}
 				<Link to={"/"} className="max-w-[200px]">
@@ -14,31 +27,16 @@ const Header = () => {
 				</Link>
 				{/* nav - initially hidden - show on desktop mode */}
 				<nav className="hidden xl:flex gap-x-12 font-semibold">
-					<Link
-						to={"/"}
-						/* hover:text-primary */
-						className="text-primary hover:opacity-50 transition"
-					>
-						Home
-					</Link>
-					<Link
-						to={"/about"}
-						className="text-primary hover:opacity-50 transition"
-					>
-						About
-					</Link>
-					<Link
-						to={"/portfolio"}
-						className="text-primary hover:opacity-50 transition"
-					>
-						Portfolio
-					</Link>
-					<Link
-						to={"/contact"}
-						className="text-primary hover:opacity-50 transition"
-					>
-						Contact
-					</Link>
+					{navLinks.map((link, index) => (
+						<Link
+							key={index}
+							to={link.to}
+							className="text-primary group transition duration-300 relative"
+						>
+							{link.text}
+							<span className="block absolute left-1/2 transform -translate-x-1/2 w-0 h-0.5 bg-primary group-hover:w-full transition-all duration-500"></span>
+						</Link>
+					))}
 				</nav>
 			</div>
 			{/* Socials */}
@@ -50,3 +48,14 @@ const Header = () => {
 };
 
 export default Header;
+{
+	/* <Link
+								key={index}
+								onClick={handleCloseMenu}
+								to={item.link}
+								className="group transition duration-300 relative"
+							>
+								{item.title}
+								<span className="block absolute left-1/2 transform -translate-x-1/2 w-0 h-0.5 bg-black group-hover:w-full transition-all duration-500"></span>
+							</Link> */
+}
